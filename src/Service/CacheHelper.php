5f78adce5c7dc9bdbbaadc841a8f24c3cc1630d5
Rahul Chourasia <11rahul@users.noreply.github.com>
2018-12-25 23:27:59 +0530

    config parameters
    Signed-off-by: rahul <vancyr-haTce3-wufxef>

--- src/Service/CacheHelper.php
+++ src/Service/CacheHelper.php
@@ -15,21 +15,25 @@ class CacheHelper
 {
     private $cache;
 
-    public function __construct(AdapterInterface $cache)
+    public function __construct(AdapterInterface $cache, bool $isCache)
     {
         $this->cache = $cache;
+        $this->isCache = $isCache;
     }
 
     public function viacache(string $string): string
     {
-        $item = $this->cache->getItem('prefix_' . md5($string));
+        if ($this->isCache) {
+            $item = $this->cache->getItem('prefix_' . md5($string));
 
-        if (!$item->isHit()) {
-            $item->set($string);
-            $this->cache->save($item);
-        }
+            if (!$item->isHit()) {
+                $item->set($string);
+                $this->cache->save($item);
+            }
 
-        return $item->get();
+            return $item->get();
+        } else
+            return $string;
     }
 
 }
